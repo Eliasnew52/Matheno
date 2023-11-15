bd697b85ac8021096cb34ab38f7b9992da9ab604
555isa <isaacvelasquez2004555@gmail.com>
2023-11-15 13:17:56 -0600

    estructura de Flask + DB

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from cs50 import SQL
 app = Flask(__name__)
+
+db = SQL("sqlite///matheno.db")
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+@app.route('/register.html')
+def register():
+    user = request.args.post('user')
+    email = request.args.post('email')
+    password = request.args.post('password')
+    return render_template('register.html')
+
+@app.route('')
+def login():
+    email = request.args.post('email')
+    password = request.args.post('password')
+    return render_template('login.html')
+
+@app.route('/users/<string:user>', methods=['POST'])
+def user():
+    return render_template('user.html', user=user)
