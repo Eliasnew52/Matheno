caf3a997fc9644360af1f7ed25d53fe9b892b6c1
555isa <isaacvelasquez2004555@gmail.com>
2023-11-19 23:01:37 -0600

    helpers

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
-from flask import Flask, render_template,request
+import os
+
+from flask import Flask, redirect, render_template,request, session
+from flask_session import Session
 from cs50 import SQL
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
+
 app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 db = SQL("sqlite///matheno.db")
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/register.html')
+@app.route("/register", methods=["POST"])
 def register():
-    user = request.args.post('user')
-    email = request.args.post('email')
-    password = request.args.post('password')
-    return render_template('register.html')
+    return render_template("register.html")
 
-@app.route('')
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    email = request.args.post('email')
-    password = request.args.post('password')
-    return render_template('login.html')
+    return render_template("login.html")
 
-@app.route('/users/<string:user>', methods=['POST'])
-def user():
-    return render_template('user.html', user=user)
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
