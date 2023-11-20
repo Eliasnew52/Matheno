c51a1222a0db2a013ac9d0b89fa511857931a94a
555isa <isaacvelasquez2004555@gmail.com>
2023-11-20 15:03:59 -0600

    login

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
+import sqlite3
 
-from flask import Flask, redirect, render_template,request, session
+from flask import Flask, redirect, render_template,request, session, flash
 from flask_session import Session
-from cs50 import SQL
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from helpers import login_required
 
 app = Flask(__name__)
@@ -13,8 +12,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-db = SQL("sqlite///matheno.db")
 
 @app.after_request
 def after_request(response):
@@ -28,13 +25,80 @@ def after_request(response):
 def index():
     return render_template("index.html")
 
-@app.route("/register", methods=["POST"])
+@app.route("/register", methods=[ "GET", "POST"])
 def register():
-    return render_template("register.html")
+    session.clear()
+    db = sqlite3.connect("matheno.db", check_same_thread=False)
+    c = db.cursor()
+    if request.method == "POST":
+        if not request.form.get("Usuario"):
+             flash("must provide username")
+             return redirect("/register")
+        elif not request.form.get("Password"):
+            flash("must provide password")
+            return redirect("/register")
+        elif not request.form.get("confirmation"):
+            flash("must confirm password")
+            return redirect("/register")
+        elif request.form.get("Password") != request.form.get("confirmation"):
+            flash("passwords must match")
+            return redirect("/register")
+        elif len(request.form.get("Password")) < 8:
+            flash("password must be at least 8 characters")
+            return redirect("/register")
+        elif request.form.get("Password").isalpha():
+            flash("password must contain at least one number or symbol")
+            return redirect("/register")
+        elif request.form.get("Password").isdigit():
+            flash("password must contain at least one letter or symbol")
+            return redirect("/register")
+
+        search = c.execute("SELECT * FROM Usuarios WHERE Usuario = ?", [request.form.get("Usuario")]).fetchall()
+        if len(search) != 0:
+            flash("username already taken")
+            return redirect("/register")
+        c.execute("INSERT INTO Usuarios (Usuario, Email, Password) VALUES (?, ?, ?)", [request.form.get("Usuario"), request.form.get("Email"), generate_password_hash(request.form.get("Password"))])
+
+        remember = db.execute("SELECT IdUsuarios FROM Usuarios WHERE Usuario = ?", [request.form.get("Usuario")]).fetchall()
+        session["user_id"] = [remember[0], ["IdUsuarios"]]
+        db.commit()
+        db.close()
+
+        return redirect("/")
+    else:
+        return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    session.clear()
+
+    db = sqlite3.connect("matheno.db", check_same_thread=False)
+    c = db.cursor()
+    if request.method == "POST":
+        if not request.form.get("Usuario"):
+            flash("must provide username")
+            return redirect("/login")
+        elif not request.form.get("password"):
+            flash("must provide password")
+            return redirect("/login")
+        
+        search = db.execute("SELECT * FROM Usuarios WHERE Usuario = ?", [request.form.get("Usuario")]).fetchall()
+
+        if len(search) == 0:
+            flash("invalid username")
+            return redirect("/login")
+        elif not check_password_hash(search[0][3], request.form.get("password")):
+            flash("invalid password")
+            return redirect("/login")
+        
+        remember = db.execute("SELECT IdUsuarios FROM Usuarios WHERE Usuario = ?", [request.form.get("Usuario")]).fetchall()
+        session["user_id"] = [remember[0], ["IdUsuarios"]]
+        db.commit()
+        db.close()
+        return redirect("/")
+    
+    else:
+        return render_template("login.html")
 
 @app.route("/logout")
 def logout():
